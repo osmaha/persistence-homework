db65847319312c059620097e1e1a345e5a7c7fae
Oleh Smaha <smahaooleh@gmail.com>
2024-09-30 19:13:40 +0300

    add 2-jpa-hibernate-homework exercise (completed)

--- 2-jpa-hibernate-homework/src/main/java/com/verong/demo/database/hibernate/persistence/model/Student.java
+++ 2-jpa-hibernate-homework/src/main/java/com/verong/demo/database/hibernate/persistence/model/Student.java
@@ -1,5 +1,6 @@
 package com.verong.demo.database.hibernate.persistence.model;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDate;
@@ -9,12 +10,31 @@ import java.time.LocalDate;
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
 @Builder
+@Entity
+@Table(name = "students")
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_seq_generator")
+    @SequenceGenerator(name = "student_seq_generator", sequenceName = "students_id_seq", allocationSize = 1)
     private Long id;
+
+    @Column(name = "email", nullable = false)
     private String email;
+
+    @Column(name = "first_name", nullable = false)
     private String firstName;
+
+    @Column(name = "last_name", nullable = false)
     private String lastName;
+
+    @Column(name = "birthday", nullable = false)
     private LocalDate birthday;
+
+    @Column(name = "scholarship", nullable = false)
     private Boolean scholarship;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "status", nullable = false)
     private StudentStatus status;
 }
